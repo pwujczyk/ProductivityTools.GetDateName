5c13081079fb90ae71ed42d3d8130e4b1c1726a3
Pawel Wujczyk <pwujczyk@hotmail.com>
2018-12-26 11:05:28 +0100

    parseexact

--- ProductivityTools.PSGetDateName/Runner.ps1
+++ ProductivityTools.PSGetDateName/Runner.ps1
@@ -4,4 +4,7 @@ Import-Module .\ProductivityTools.PSGetDateName.psm1 -Force
 Get-DateName  -verbose
 Get-DateName -Suffix "s" -Prefix "p" -verbose
 Write-Host "####"
-Get-LastDateDirectoryName "d:\Trash\xxx\" -Verbose
+$path=Get-DateName -Suffix "s" -Prefix "p" -verbose
+$path="d:\Trash\"+$path
+New-Item -ItemType Directory -Path $path
+Get-LastDateDirectoryName "d:\Trash\" -Verbose -Prefix "p" -Suffix "s"
