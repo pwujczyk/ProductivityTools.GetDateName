a28fffc584c19b191c533912369ed0db273eaad4
Pawel Wujczyk <pwujczyk@hotmail.com>
2020-11-16 20:39:14 +0100

    rename projecT

--- ProductivityTools.GetDateName/Runner.ps1
+++ ProductivityTools.GetDateName/Runner.ps1
@@ -1,6 +1,6 @@
 Clear-Host
 cd $PSScriptRoot
-Import-Module .\ProductivityTools.PSGetDateName.psm1 -Force
+Import-Module .\ProductivityTools.GetDateName.psm1 -Force
 Get-DateName  -verbose
 Get-DateName -Suffix "s" -Prefix "p" -verbose
 Write-Host "####"
